73a912d60ba2b4fa0cd8b2adc0471fda2d3c8076
grenos <Vasilis.green@gmail.com>
2019-10-01 16:20:31 +0200

    Initial Commit

---
+++ testapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testapp
+//
+//  Created by Vasilis Green on 1/10/19.
+//  Copyright © 2019 Vasilis Green. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
